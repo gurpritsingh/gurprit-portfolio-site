74b6456dcbbd86d08334a3bc80c7e1df7951d94a
Gurprit Singh <gurprit1990@gmail.com>
2018-11-20 20:48:56 +0530

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'Gurprit | Web | Dev',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
